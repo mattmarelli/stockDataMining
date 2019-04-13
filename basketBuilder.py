e3a209654164cc40aa534fc91b3dec333ebdbb52
mmarell1 <mmarell1@eecs.wsu.edu>
2019-04-13 16:40:35 -0700

    adding preprocessing files

---
+++ basketBuilder.py
@@ -0,0 +1,48 @@
+from os import walk
+import numpy as np
+
+if __name__ == "__main__":
+    stockBaskets = {} 
+    for root, dirs, filenames in walk('StocksCleaned/'):
+        # print(filenames)
+        for stock in filenames:
+            stockName = str(stock).split('D')[0]
+            stockData = np.loadtxt(fname = 'StocksCleaned/' + str(stock), delimiter = ',', dtype = str)
+            for dataPoint in stockData:
+                dayData = stockBaskets.get(str(dataPoint[0]), {})
+                magnitudeChange = abs(dataPoint[1].astype(float))
+                if magnitudeChange < 0.5:
+                    magnitudeData = dayData.get('0-.499', [])
+                    magnitudeData.append(stockName)
+                    dayData.update({'0-.499' : magnitudeData})
+                elif magnitudeChange < 1.0:
+                    magnitudeData = dayData.get('.5-.999', [])
+                    magnitudeData.append(stockName)
+                    dayData.update({'.5-.999' : magnitudeData}) 
+                elif magnitudeChange < 1.5:
+                    magnitudeData = dayData.get('.999-1.49', [])
+                    magnitudeData.append(stockName)
+                    dayData.update({'.999-1.49' : magnitudeData})
+                elif magnitudeChange < 2.0:
+                    magnitudeData = dayData.get('1.49-1.99', [])
+                    magnitudeData.append(stockName)
+                    dayData.update({'1.49-1.99' : magnitudeData})
+                elif magnitudeChange < 2.5:
+                    magnitudeData = dayData.get('1.99-2.49', [])
+                    magnitudeData.append(stockName)
+                    dayData.update({'1.99-2.49' : magnitudeData})
+                elif magnitudeChange < 3.0:
+                    magnitudeData = dayData.get('2.49-2.99', [])
+                    magnitudeData.append(stockName)
+                    dayData.update({'2.49-2.99' : magnitudeData})
+                else:
+                    magnitudeData = dayData.get('3.00+', [])
+                    magnitudeData.append(stockName)
+                    dayData.update({'3.00+' : magnitudeData})                            
+                stockBaskets.update({str(dataPoint[0]) : dayData})
+    # print(stockBaskets)
+    for key in stockBaskets.keys():
+        for secondKey in sorted(stockBaskets.get(key)):
+            print(str(key) + ", " +  str(secondKey) + ": " + str(stockBaskets.get(key).get(secondKey)))
+        
+            
---
+++ percentChangeFinder.py
@@ -0,0 +1,47 @@
+import numpy as np
+from os import walk
+
+
+
+if __name__ == "__main__":
+    startYear = 1999
+    endYear = 2005
+    for root, dirs, filenames in walk('Stocks/'):
+        for stock in filenames:
+            stockData = np.loadtxt(fname = 'Stocks/' + str(stock), delimiter = ',', dtype = str)
+            stockData = stockData[1:]
+            date = stockData[:,0]
+            count = 0
+            validYear = False
+            oneDay = ""
+            tempList = []
+            for lineOfData in np.nditer(stockData):
+                if count % 7 == 0:
+                    oneDay = str(lineOfData)
+                    year = oneDay[:4]
+                    if int(year) < endYear and int(year) > startYear:
+                        validYear = True
+                    else:
+                        validYear = False
+                else:
+                    if validYear:
+                        if count % 7 == 1:
+                            openingPrice = str(lineOfData)
+                        if count % 7 == 4:
+                            closingPrice = str(lineOfData)
+                            dayChange = float(closingPrice) - float(openingPrice)
+                            percentChange = (dayChange / float(openingPrice)) * 100
+                            tempList.append([oneDay, percentChange])
+                        # if count % 7 == 6:
+                        #     dayChange = float(closingPrice) - float(openingPrice)
+                        #     percentChange = (dayChange / float(openingPrice)) * 100
+                        #     tempList.append([oneDay, percentChange])
+                count += 1
+            if tempList != []:
+                outputList = np.array(tempList)
+                output = np.zeros(shape= (len(tempList), 2)).astype(str)
+                output[:,0] = outputList[:,0].astype(str)
+                output[:,1] = outputList[:,1].astype(str)
+                # temp = stock.split('.')
+                fileName = "StocksCleaned/" + stock.split('.')[0] + 'DailyChange.txt'
+                np.savetxt(fname = fileName, X = output, fmt='%s, %s', delimiter= ',', encoding = 'utf8')
