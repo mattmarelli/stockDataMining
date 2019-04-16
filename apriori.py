6026aaaa34e512ebf1c4dc4b0692c778e4c3fbb0
Ian-Pruett <ianpruett14@gmail.com>
2019-04-16 10:17:38 -0700

    made a bit more progress on apriori

--- apriori.py
+++ apriori.py
@@ -8,9 +8,20 @@ import scipy as sp
 # and s is support for k tuples
 def apriori(D,s,k):
     # instantiate candidate set and count
-    itemset,itemcount = np.unique( D,
-        return_counts=True
+    itemset,itemcount = np.unique( 
+        D,return_counts=True
     )
+
+    # helper function, returns the count of a 
+    # set s as s appears in each row of itemset
+    def contains(s):
+        foundrows = [
+            set(row).issuperset(s) 
+            for row in itemset.tolist()
+        ]
+        count = len(np.where(foundrows)[0])
+        return count
+
     # find cases that statisy support threshold
     np.where(itemcount < s, 0, itemcount)
     # items that meet support, frequent singles
@@ -19,13 +30,18 @@ def apriori(D,s,k):
     i = 2 # start i = 2 to build candidate set
 
     while i < k:
+        # generate all possible combinations of 
+        # remaining items in itemset
         canidates = np.fromiter(
             chain(combinations(itemset,i))
         )
-        # TODO count the occurances in D of each
-        # subset in candidate set and assign to itemset
+        itemcount = np.zeros(shape=len(canidates))
 
-     
+        # assign the count of each subset to itemcount
+        for r in len(canidates):
+            itemcount[r] = contains(r)
+        
+        # TODO figure out indexing of subsets and itemsets
 
 
 # TODO additional preprocessing dataset
